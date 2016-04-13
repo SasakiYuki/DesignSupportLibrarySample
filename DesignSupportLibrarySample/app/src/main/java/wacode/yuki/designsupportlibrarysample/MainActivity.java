293635cd051113bfecb2c703aa2127106bafc732
SasakiYuki <sasakimk6@gmail.com>
2016-04-14 08:55:37 +0900

    first commit

---
+++ DesignSupportLibrarySample/app/src/main/java/wacode/yuki/designsupportlibrarysample/MainActivity.java
@@ -0,0 +1,13 @@
+package wacode.yuki.designsupportlibrarysample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

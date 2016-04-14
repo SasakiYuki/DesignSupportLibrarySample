dcfa1fbfaf49b698db3e59073402e50722fca317
SasakiYuki <sasakimk6@gmail.com>
2016-04-14 20:00:16 +0900

    configure:MainActivity button intent

--- DesignSupportLibrarySample/app/src/main/java/wacode/yuki/designsupportlibrarysample/MainActivity.java
+++ DesignSupportLibrarySample/app/src/main/java/wacode/yuki/designsupportlibrarysample/MainActivity.java
@@ -3,16 +3,32 @@ package wacode.yuki.designsupportlibrarysample;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import wacode.yuki.designsupportlibrarysample.kotCode.NavigationView.ktNavigationViewActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, ktNavigationViewActivity.class);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch (view.getId()){
+            case R.id.button_kot_navigation:
+                intent = new Intent(this,ktNavigationViewActivity.class);
+                break;
+            default:
+                intent = null;
+        }
         startActivity(intent);
     }
 }
